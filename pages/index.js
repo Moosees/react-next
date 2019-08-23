73ec5ae002ae4ddccb85231ea438734d19c9543d
Moosees <linus.almgren@gmail.com>
2019-08-23 23:42:27 +0200

    getInitialProps and fetching data

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
+import axios from 'axios';
 import Link from 'next/link';
 
-const HomePage = () => (
-  <div>
-    <h1>Homepage!</h1>
-  </div>
-);
+const HomePage = ({ posts }) => {
+  return (
+    <div>
+      <h1>Homepage!</h1>
+      <div>{posts[Math.floor(Math.random() * 100)].title}</div>
+    </div>
+  );
+};
+
+HomePage.getInitialProps = async ({ req }) => {
+  const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  return { posts: res.data };
+};
 
 export default HomePage;
